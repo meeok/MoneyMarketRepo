45b13c4cf7499483042f6433611e52f8aa75ce9b
Kel Morgan <kelmorgan18@gmail.com>
2021-04-29 21:49:08 +0100

    code from idea home laptop

--- MoneyMarketW/src/com/newgen/worksteps/TreasuryOpsVerifier.java
+++ MoneyMarketW/src/com/newgen/worksteps/TreasuryOpsVerifier.java
@@ -1,5 +1,6 @@
 package com.newgen.worksteps;
 
+import com.newgen.controller.CpController;
 import com.newgen.iforms.EControl;
 import com.newgen.iforms.FormDef;
 import com.newgen.iforms.custom.IFormReference;
@@ -7,12 +8,15 @@ import com.newgen.iforms.custom.IFormServerEventHandler;
 import com.newgen.utils.Commons;
 import com.newgen.utils.CommonsI;
 import com.newgen.utils.Constants;
+import com.newgen.utils.LogGen;
+import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TreasuryOpsVerifier extends Commons implements IFormServerEventHandler, Constants, CommonsI {
+    private static Logger logger = LogGen.getLoggerInstance(TreasuryOpsVerifier.class);
     @Override
     public void beforeFormLoad(FormDef formDef, IFormReference ifr) {
         clearDecHisFlag(ifr);
@@ -33,7 +37,53 @@ public class TreasuryOpsVerifier extends Commons implements IFormServerEventHand
     }
 
     @Override
-    public String executeServerEvent(IFormReference iFormReference, String s, String s1, String s2) {
+    public String executeServerEvent(IFormReference ifr, String control, String event, String data) {
+
+        try{
+            switch (event){
+                case cpApiCallEvent:{
+                    switch (control) {
+                        case cpTokenEvent: return CpController.tokenController(ifr);
+                        case cpPostEvent:{
+                            if (getCpMarket(ifr).equalsIgnoreCase(cpPrimaryMarket)) {
+                                if (cpCheckWindowStateById(ifr, getCpPmWinRefNoBr(ifr))) return CpController.postTranController(ifr);
+                                else return cpValidateWindowErrorMsg;
+                            }
+                            else if (getCpMarket(ifr).equalsIgnoreCase(cpSecondaryMarket)) {
+                                if (cpCheckWindowStateById(ifr, getCpSmWinRefNoBr(ifr))) return CpController.postTranController(ifr);
+                                else return cpValidateWindowErrorMsg;
+                            }
+                        }
+                    }
+                }
+                case formLoad:
+                case onLoad:
+                case onClick:{
+                    switch (control){
+
+                    }
+                }
+                case onChange:
+                case custom:
+                case onDone:{
+                    switch (control){
+
+                    }
+                }
+                break;
+                case decisionHistory: {
+                    if (getProcess(ifr).equalsIgnoreCase(commercialProcess))
+                        setCpDecisionHistory(ifr);
+                    else if (getProcess(ifr).equalsIgnoreCase(treasuryProcess))
+                        setTbDecisionHistory(ifr);
+                }
+                break;
+                case sendMail:
+            }
+        }
+        catch (Exception e){
+            logger.error("Exception occurred-- "+ e.getMessage());
+        }
         return null;
     }
 
@@ -73,7 +123,19 @@ public class TreasuryOpsVerifier extends Commons implements IFormServerEventHand
         hideShowLandingMessageLabel(ifr,False);
         hideShowBackToDashboard(ifr,False);
         clearFields(ifr,new String[]{cpRemarksLocal,cpDecisionLocal});
+
         if (getPrevWs(ifr).equalsIgnoreCase(branchVerifier)){
+          if (getCpMandateType(ifr).equalsIgnoreCase(cpMandateTypeTerminate)){
+              setMandatory(ifr,new String[]{cpDecisionLocal,cpRemarksLocal,cpTokenLocal});
+              enableFields(ifr,new String[]{cpDecisionLocal,cpRemarksLocal,cpTokenLocal});
+              setVisible(ifr,new String[]{cpDecisionSection,cpTerminationSection,cpTermMandateTbl,cpTermSpecialRateLocal, getCpTermIsSpecialRate(ifr) ? cpTermSpecialRateValueLocal : cpTermSpecialRateLocal});
+              if (getCpTerminationType(ifr).equalsIgnoreCase(cpTerminationTypeFull)){
+                  setVisible(ifr,new String[]{cpTermAmountDueLocal});
+              }
+              else if (getCpTerminationType(ifr).equalsIgnoreCase(cpTerminationTypePartial)){
+                  setVisible(ifr,new String[]{cpTermAmountDueLocal,cpTermAdjustedPrincipalLocal,cpTermPartialOptionLocal,cpTermPartialAmountLocal,cpTermPartialOptionLocal});
+              }
+          }
 
         }
 
